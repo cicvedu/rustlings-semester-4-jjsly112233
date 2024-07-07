6f15f57906990f37d1dd5016d6ce30395533246e
jjsly112233 <137158422+jjsly112233@users.noreply.github.com>
2024-07-07 05:05:46 +0000

    0707

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,13 +11,13 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
-
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
+    basket.insert(String::from("apple"), 5);
+    basket.insert(String::from("orange"), 4);
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,11 +14,10 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq,Debug,Clone)]
 enum Fruit {
     Apple,
     Banana,
@@ -36,10 +35,13 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Pineapple,
     ];
 
-    for fruit in fruit_kinds {
+    for fruit in &fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if !basket.contains_key(&fruit){
+            basket.insert(fruit.clone(),1);
+        }
     }
 }
 
